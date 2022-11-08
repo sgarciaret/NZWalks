a110459d976354b5aba1cda7dc1ca7995380971e
sgarcia <sg_live7@hotmail.com>
2022-11-08 17:07:31 +0100

    End of section 2

---
+++ NZWalks/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Models.Domain;
+
+namespace NZWalks.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+
+        // With this Entity Framework creates a table if it doesn't exist
+        // Setting it as a DbSet so we can use the context and query or persist data to the tables
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ NZWalks/Migrations/20221108160404_InitMgration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalksAPI.Migrations
+{
+    public partial class InitMgration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Lenght",
+                table: "Walks",
+                newName: "Length");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Length",
+                table: "Walks",
+                newName: "Lenght");
+        }
+    }
+}
